3f1079db5952336c31a2511ad185a1ea37dde6b8
Ulises Carrasco <ucarrascof@gmail.com>
2023-06-22 12:27:38 -0600

    Agregando librerias python dotenv y openai

---
+++ chatboot.py
@@ -0,0 +1,12 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = api_key
+
+modelos = openai.Model.list()
+print(modelos)
+
